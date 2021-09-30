2e5f01ca1fa14314563cada00cb55687c6234d3e
Warodom WERAPUN <wwarodom@gmail.com>
2021-09-30 12:11:05 +0700

    Add ViteJS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,75 @@
+import { ethers } from "ethers";
+import { useState } from "react";
+import { Web3Provider, JsonRpcSigner, JsonRpcProvider } from "@ethersproject/providers"
+
+import { address } from './contracts/address.json';
+import { abi } from './contracts/abi.json';
+import { Greeter } from "../../src/types/Greeter";
+
+
+let provider: Web3Provider| JsonRpcProvider;
+let signer: JsonRpcSigner
+let greetContract: Greeter;
+
+function App() {
+
+  const [addressWallet, setAddressWallet] = useState('0x');
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+
+  async function connect() {
+    // provider = new ethers.providers.Web3Provider(window.ethereum)
+    // await provider.send('eth_requstAcounts',[]); // ethereum.enable()
+
+    provider =  new ethers.providers.JsonRpcProvider();
+    signer = provider.getSigner();
+
+    setAddressWallet(await signer.getAddress())
+    console.log('signer: ', signer);
+
+    // create contract instace
+    greetContract = new ethers.Contract(address, abi, signer) as Greeter;
+
+  }
+
+  async function greet() {
+    if (greetContract === undefined)
+      setStatus('Please firstly connect wallet');
+    else
+      setStatus(await greetContract.greet())
+  }
+
+  async function setGreet() {
+    if (greetContract === undefined)
+      setStatus('Please firstly connect wallet');
+    else {
+      const tx = await greetContract.connect(signer).setGreeting(message);
+      await tx.wait();
+      setStatus('Write to block successfully')
+    }
+
+     
+  }
+
+  return (
+    <div>
+      <div>
+        Address: {addressWallet}
+      </div>
+      <div>
+        Status: {status}
+      </div>
+      <div>
+        <button onClick={() => connect()}>Connect</button>
+      </div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+
+      <div>
+        <button onClick={() => setGreet()}>set</button>
+        <button onClick={() => greet()}>Get</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- scripts/sample-script.ts
+++ scripts/sample-script.ts
@@ -1,15 +1,26 @@
 
-import {ethers} from "hardhat";
-import {Greeter} from "../src/types/Greeter";
-
+import fs from "fs";
+import { ethers, artifacts } from "hardhat";
+import { Greeter } from "../src/types/Greeter";
 
 async function main() {
-  const Greeter = await  ethers.getContractFactory("Greeter");
-  const greeter:Greeter = await Greeter.deploy("Hello, Hardhat!") as Greeter;
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter: Greeter = await Greeter.deploy("Hello, Hardhat!") as Greeter;
 
   await greeter.deployed();
-  console.log("Foo:" , await greeter.foo(34));
+  console.log("Foo:", await greeter.foo(34));
   console.log("Greeter deployed to:", greeter.address);
+  saveContract(greeter);
+}
+
+function saveContract(greeter: Greeter) {
+  const path = __dirname + '/../frontend/src/contracts';
+  if (!fs.existsSync(path))
+    fs.mkdirSync(path);
+  fs.writeFileSync(`${path}/address.json`,
+    JSON.stringify({ address: greeter.address }, undefined, 2))
+  fs.writeFileSync(`${path}/abi.json`,
+    JSON.stringify( artifacts.readArtifactSync('Greeter'), undefined ,2) )
 }
 
 main()
